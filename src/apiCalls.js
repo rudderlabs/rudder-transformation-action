1efcf5cd15f9c6ab3fb2d9c8243fa26fa2afeba3
sayan-mitra <sayan.besu@gmail.com>
2021-04-08 12:40:46 +0530

    add test and publish feature

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,13 +4,49 @@ const serverEndpoint = core.getInput("serverEndpoint");
 const createTransformerEndpoint = `${serverEndpoint}/transformations`;
 const createLibraryEndpoint = `${serverEndpoint}/libraries`;
 const testEndpoint = `${serverEndpoint}/transformations/libraries/test`;
+const publishEndpoint = `${serverEndpoint}/transformations/libraries/publish`;
+const listTransformationsEndpoint = `${serverEndpoint}/transformations`;
+const listLibrariesEndpoint = `${serverEndpoint}/libraries`;
+
+async function getAllTransformations() {
+  return axios.default.get(listTransformationsEndpoint, {
+    auth: {
+      username: core.getInput("email"),
+      password: core.getInput("accessKey")
+    }
+  });
+}
+
+async function getAllLibraries() {
+  return axios.default.get(listLibrariesEndpoint, {
+    auth: {
+      username: core.getInput("email"),
+      password: core.getInput("accessKey")
+    }
+  });
+}
 
 async function createTransformer(name, description, code) {
-  //console.log(code)
   return axios.default.post(
     `${createTransformerEndpoint}?publish=false`,
     {
       name,
+      description,
+      code
+    },
+    {
+      auth: {
+        username: core.getInput("email"),
+        password: core.getInput("accessKey")
+      }
+    }
+  );
+}
+
+async function updateTransformer(id, description, code) {
+  return axios.default.post(
+    `${createTransformerEndpoint}/${id}?publish=false`,
+    {
       description,
       code
     },
@@ -40,6 +76,22 @@ async function createLibrary(name, description, code) {
   );
 }
 
+async function updateLibrary(id, description, code) {
+  return axios.default.post(
+    `${createLibraryEndpoint}/${id}?publish=false`,
+    {
+      description,
+      code
+    },
+    {
+      auth: {
+        username: core.getInput("email"),
+        password: core.getInput("accessKey")
+      }
+    }
+  );
+}
+
 async function testTransformationAndLibrary(transformations, libraries) {
   return axios.default.post(
     `${testEndpoint}`,
@@ -56,8 +108,29 @@ async function testTransformationAndLibrary(transformations, libraries) {
   );
 }
 
+async function publish(transformations, libraries) {
+  return axios.default.post(
+    `${publishEndpoint}`,
+    {
+      transformations,
+      libraries
+    },
+    {
+      auth: {
+        username: core.getInput("email"),
+        password: core.getInput("accessKey")
+      }
+    }
+  );
+}
+
 module.exports = {
+  getAllTransformations,
+  getAllLibraries,
   createTransformer,
   createLibrary,
-  testTransformationAndLibrary
+  updateTransformer,
+  updateLibrary,
+  testTransformationAndLibrary,
+  publish
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const core = require("@actions/core");
 const fs = require("fs");
+const isEqual = require("lodash/isEqual");
+const artifact = require("@actions/artifact");
+const artifactClient = artifact.create();
 const {
+  getAllTransformations,
+  getAllLibraries,
   createTransformer,
   createLibrary,
-  testTransformationAndLibrary
+  updateTransformer,
+  updateLibrary,
+  testTransformationAndLibrary,
+  publish
 } = require("./apiCalls");
 
+const uploadTestArtifact = core.getInput("uploadTestArtifact") || false;
 const metaFilePath = core.getInput("metaPath");
 
 const { transformations, libraries } = JSON.parse(
@@ -14,42 +23,77 @@ const { transformations, libraries } = JSON.parse(
 console.log("---transformations---", transformations);
 console.log("----libraries---", libraries);
 
-async function test() {
+const serverList = {
+  transformations: [],
+  libraries: []
+};
+
+const transformationNameToId = {};
+const libraryNameToId = {};
+
+function buildNametoIdMap(objectArr, type) {
+  if (type == "tr") {
+    objectArr.map(tr => {
+      transformationNameToId[tr.name] = tr.id;
+    });
+  } else {
+    objectArr.map(lib => {
+      libraryNameToId[lib.name] = lib.id;
+    });
+  }
+}
+
+async function init() {
+  let res = await getAllTransformations();
+  serverList.transformations = res.data
+    ? JSON.parse(JSON.stringify(res.data.transformations))
+    : [];
+  res = await getAllLibraries();
+  serverList.libraries = res.data ? JSON.parse(JSON.stringify(res.data)) : [];
+  buildNametoIdMap(serverList.transformations, "tr");
+  buildNametoIdMap(serverList.libraries, "lib");
+}
+
+async function testAndPublish() {
   const transformationDict = {};
   const libraryDict = {};
+
+  await init();
 
   try {
     for (let i = 0; i < transformations.length; i++) {
       let tr = transformations[i];
       let code = fs.readFileSync(tr.file, "utf-8");
-      let res = await createTransformer(tr.name, tr.description, code);
+      let res;
+      if (transformationNameToId[tr.name]) {
+        // update existing transformer and get a new versionId
+        let id = transformationNameToId[tr.name];
+        res = await updateTransformer(id, tr.description, tr.code);
+      } else {
+        // create new transformer
+        res = await createTransformer(tr.name, tr.description, code);
+      }
       transformationDict[res.data.versionId] = { ...tr, id: res.data.id };
       console.log("creating transformation");
     }
-
-    // await transformations.forEach(async tr => {
-    //   let code = fs.readFileSync(tr.file, "utf-8");
-    //   let res = await createTransformer(tr.name, tr.description, code);
-    //   transformationDict[res.data.versionId] = { ...tr, id: res.data.id };
-    //   console.log("creating transformation");
-    // });
 
     console.log("transformation done!");
 
     for (let i = 0; i < libraries.length; i++) {
       let lib = libraries[i];
       let code = fs.readFileSync(lib.file, "utf-8");
-      let res = await createLibrary(lib.name, lib.description, code);
+      let res;
+      if (libraryNameToId[lib.name]) {
+        // update library and get a new versionId
+        let id = libraryNameToId[lib.name];
+        res = await updateLibrary(id, lib.description, code);
+      } else {
+        // create a new library
+        res = await createLibrary(lib.name, lib.description, code);
+      }
       libraryDict[res.data.versionId] = { ...lib, id: res.data.id };
       console.log("creating library");
     }
-
-    // await libraries.forEach(async lib => {
-    //   let code = fs.readFileSync(lib.file, "utf-8");
-    //   let res = await createLibrary(lib.name, lib.description, code);
-    //   libraryDict[res.data.versionId] = { ...lib, id: res.data.id };
-    //   console.log("creatingg library");
-    // });
 
     console.log("library done!");
 
@@ -63,19 +107,9 @@ async function test() {
       transformationTest.push({ versionId: trVersionId, testInput });
     }
 
-    // await Object.keys(transformationDict).forEach(async trVersionId => {
-    //   let testInputPath = transformationDict[trVersionId]["test-input-file"];
-    //   let testInput = JSON.parse(fs.readFileSync(testInputPath));
-    //   transformationTest.push({ versionId: trVersionId, testInput });
-    // });
-
     for (let i = 0; i < Object.keys(libraryDict).length; i++) {
       librariesTest.push({ versionId: Object.keys(libraryDict)[i] });
     }
-
-    // Object.keys(libraryDict).forEach(libVersionId => {
-    //   librariesTest.push({ versionId: libVersionId });
-    // });
 
     console.log("final transformation------", transformationTest);
     console.log("final library-----", librariesTest);
@@ -85,13 +119,58 @@ async function test() {
       librariesTest
     );
     console.log(JSON.stringify(res.data));
+
+    // upload artifact
+    if (uploadTestArtifact) {
+      fs.writeFileSync("test-results.json", JSON.stringify(res.data));
+      await artifactClient.uploadArtifact(
+        "transformer-test-results",
+        ["test-results.json"],
+        "."
+      );
+    }
+
+    if (res.data.result.failedTestResults.length > 0) {
+      throw new Error(
+        "There are failures in running the set aggainst input events"
+      );
+    }
+
+    let successResults = res.data.result.successTestResults;
+
+    for (let i = 0; i < successResults.length; i++) {
+      let expectedOutputfile =
+        transformationDict[successResults[i].transformerVersionID][
+          "expected-output"
+        ];
+      let expectedOutput = expectedOutputfile
+        ? JSON.parse(fs.readFileSync(expectedOutputfile))
+        : "";
+      if (expectedOutput == "") {
+        continue;
+      }
+      let apiOutput = successResults[i].result.output;
+      if (!isEqual(expectedOutput, apiOutput)) {
+        throw new Error(
+          `${successResults[i].transformerVersionID} outputs don't match`
+        );
+      }
+    }
+
+    // test passed
+    console.log("Test Passed!!!");
+
+    // publish
+
+    res = await publish(transformationTest, librariesTest);
+    console.log(res.data);
   } catch (err) {
     if (err.response) {
       console.log(err.response.data);
       console.log(err.response.status);
-      core.error(err);
+      core.setFailed(err);
     }
   }
 }
 
-test();
+testAndPublish();
